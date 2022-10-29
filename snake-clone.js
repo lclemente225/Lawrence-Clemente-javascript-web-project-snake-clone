25a6e9cd71831c3720fcc27f0fa92c88933e1415
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2022-10-28 17:15:00 -0700

    second draft

--- snake-clone.js
+++ snake-clone.js
@@ -110,14 +110,18 @@ document.addEventListener('DOMContentLoaded', () => {
         currentSnake = [2,1,0];
         currentIndex = 0;
         currentSnake.forEach(index => squares[index].classList.add('snake'));
+        currentSnake[0].classList.add('bro');
         interval = setInterval(moveOutcomes, intervalTime);    
         console.log(squares.parentNode);  
         console.log("starting YES");
+             let bro = document.getElementsByClassName("bro");
+             console.log(bro);
+             bro.style.backgroundImage = "url('gayce.png')";
+    }   
 
-    }
-
-
+  
     //function that deals with All the move outcomes of the Snake
+
 
     function moveOutcomes(){
     //deals with snake hitting the border and self
